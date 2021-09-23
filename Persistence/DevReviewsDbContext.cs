8329bff23a8d0069f3343487107de374c9e931f5
TI <ti@ARN-LAB04-PC04.ifto.local>
2021-09-23 18:05:36 -0300

    banco de dados e persistence

--- Persistence/DevReviewsDbContext.cs
+++ Persistence/DevReviewsDbContext.cs
@@ -15,7 +15,33 @@ namespace DevReviews.API.Persistence
         public DbSet<ProductReview> ProductReviews { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder){
-            
+            /* modelBuilder.Entity<Product>()
+            .ToTable("tb_Product");
+
+            modelBuilder.Entity<Product>()
+            .HasKey(p => p.Id); */
+
+            modelBuilder.Entity<Product>(p => {
+                p.ToTable("tb_Product");
+                p.HasKey(p => p.Id);
+
+                p.HasMany(pp => pp.Reviews)
+                 .WithOne()
+                 .HasForeignKey(r => r.ProductId)
+                 .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            /* modelBuilder.Entity<ProductReview>()
+            .ToTable("tb_ProductReview"); */
+
+            modelBuilder.Entity<ProductReview>(pr => {
+                pr.ToTable("tb_ProductReview");
+                pr.HasKey(p => p.Id);
+
+                pr.Property(p => p.Author) // restrições
+                .HasMaxLength(50)
+                .IsRequired(); // o atributo author vai ter no máximo 50 caracteres
+            });
         }
         
         
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -31,7 +32,7 @@ namespace DevReviews.API
             var ConfigurationString = Configuration.GetValue<String>("DevReviewsCn");
             
             // Transient, Scoped, Singleton
-            services.AddSingleton<DevReviewsDbContext>();
+            services.AddDbContext<DevReviewsDbContext>(o => o.UseSqlServer(ConfigurationString));
 
             services.AddAutoMapper(typeof(ProductProfile));
 
